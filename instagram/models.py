181c0ec19b686c912992338d873843cac48fdffb
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-03-06 19:35:01 +0300

    Registered the admin to the admin.py

--- instagram/models.py
+++ instagram/models.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 from time import timezone
 from django.db import models
 from django.utils import timezone
@@ -8,3 +7,7 @@ class Post(models.Model):
     image = models.ImageField(blank=True, null=True)
     caption = models.TextField()
     date_created = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.caption
