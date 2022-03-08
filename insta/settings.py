ad8edbdfe761a284a7de0e757d7c8664f038a656
nancy nyokabi <nancynyokabi1995@gmail.com>
2022-03-08 22:46:57 +0300

    installed django decouple

--- insta/settings.py
+++ insta/settings.py
@@ -116,7 +116,8 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
